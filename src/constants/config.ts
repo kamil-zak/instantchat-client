2aaf4ec79903ad146d4ee3b549aeba281390b4f4
Kamil Żak <kamil.zak.pl@outlook.com>
2022-05-24 13:55:57 +0200

    Configure env variables

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,10 +1,8 @@
 export const QUERY_MESSAGES_COUNT = 25;
 
-export const GRAPHQL_URL = import.meta.env.PROD ? '/graphql' : 'http://localhost:3434/graphql';
-
-export const WS_URL = import.meta.env.PROD
-  ? window.location.href.replace(/^http(s?:\/\/.*?)\/.*$/, `ws$1/graphql`)
-  : 'ws://localhost:3434/graphql';
+export const SERVER = import.meta.env.VITE_SERVER_HTTP;
+export const GRAPHQL_URL = `${SERVER}/graphql`;
+export const WS_URL = `${import.meta.env.VITE_SERVER_WS}/graphql`;
 
 export const ChatBoxButtonDimensions = { width: 100, height: 70 };
 export const ChatBoxDimensions = { width: 300, height: 500 };
--- src/pages/Chats/components/ChatBoxItem/ChatBoxItem.tsx
+++ src/pages/Chats/components/ChatBoxItem/ChatBoxItem.tsx
@@ -1,16 +1,16 @@
 import Button from '../../../../components/Button/Button';
 import StyledText from '../../../../components/StyledText/StyledText';
+import { SERVER } from '../../../../constants/config';
 import { IChat } from '../../../../interfaces/chat';
 import { ChatBoxItemWrapper } from './ChatBoxItem.styles';
 
-const chatBoxUrl = import.meta.env.VITE_CHATBOX_URL || 'http://instantchat.kamilzak.pl/chatbox.js?chatId=';
-const getScript = (id: string) => `<script src='${chatBoxUrl}${id}'></script>`;
+const getScript = (id: string) => `<script src='${SERVER}/chatbox.js?chatId=${id}'></script>`;
 
 const ChatBoxItem = ({ id, name }: IChat) => {
   return (
     <ChatBoxItemWrapper>
       <StyledText>Name: {name}</StyledText>
-      <a href={`/chatbox/${id}`} target="_blank" rel="noreferrer">
+      <a href={`${SERVER}/chatbox/${id}`} target="_blank" rel="noreferrer">
         <Button>Open test ChatBox</Button>
       </a>
 
