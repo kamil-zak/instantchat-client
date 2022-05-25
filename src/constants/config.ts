2ca99f70053219db8c85f0224376d06e778a4253
Kamil Żak <kamil.zak.pl@outlook.com>
2022-05-25 22:54:23 +0200

    Configure proxy

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,8 +1,8 @@
 export const QUERY_MESSAGES_COUNT = 25;
 
-export const SERVER = import.meta.env.VITE_SERVER_HTTP;
-export const GRAPHQL_URL = `${SERVER}/graphql`;
-export const WS_URL = `${import.meta.env.VITE_SERVER_WS}/graphql`;
+export const SERVER = import.meta.env.VITE_SERVER;
+export const GRAPHQL_URL = `/graphql`;
+export const WS_URL = window.location.href.replace(/^http(s?:\/\/.*?)\/.*$/, `ws$1/graphql`);
 
 export const chatBoxButtonDimensions = { width: 100, height: 70 };
 export const chatBoxDimensions = { width: 300, height: 500 };
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/panel/',
+  server: {
+    proxy: {
+      '/graphql': {
+        target: 'http://localhost:3434',
+        ws: true,
+      },
+    },
+  },
   build: {
     rollupOptions: {
       input: {
