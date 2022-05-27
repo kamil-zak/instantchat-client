59e9ca8203c07f047c9fc19f4217581ccc3486be
Kamil Żak <kamil.zak.pl@outlook.com>
2022-05-27 18:44:37 +0200

    Add button modifiers

--- src/components/Button/Button.ts
+++ src/components/Button/Button.ts
@@ -1,13 +1,39 @@
-import styled from 'styled-components';
+import styled, { css, DefaultTheme } from 'styled-components';
 
-const Button = styled.button`
+interface IButtonProps {
+  bordered?: boolean;
+  color?: keyof DefaultTheme['colors'];
+}
+
+const Button = styled.button<IButtonProps>`
   padding: 10px 30px;
-  background: linear-gradient(90deg, rgb(47, 38, 167) 0%, rgb(68, 93, 192) 100%);
+  background: ${({ theme, color = 'primary' }) => theme.colors[color]};
   color: white;
   font-size: ${({ theme }) => theme.fontSizes.xs};
-  border: none;
   border-radius: 15px;
   cursor: pointer;
+  box-sizing: border-box;
+  border: 2px solid ${({ theme, color = 'primary' }) => theme.colors[color]};
+  transition: background-color 0.1s ease-in-out, color 0.1s ease-in-out;
+
+  ${({ bordered }) =>
+    bordered &&
+    css<IButtonProps>`
+      background: transparent;
+      color: ${({ theme, color = 'primary' }) => theme.colors[color]};
+    `}
+
+  &:hover {
+    background: transparent;
+    color: ${({ theme, color = 'primary' }) => theme.colors[color]};
+
+    ${({ bordered }) =>
+      bordered &&
+      css<IButtonProps>`
+        background: ${({ theme, color = 'primary' }) => theme.colors[color]};
+        color: white;
+      `}
+  }
 `;
 
 export default Button;
