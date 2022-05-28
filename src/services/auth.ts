e5b4c803626382d5b35b418404917859583f1cfc
Kamil Żak <kamil.zak.pl@outlook.com>
2022-05-28 15:43:39 +0200

    Fix refresh token error handling

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -22,11 +22,14 @@ const refresh = async () => {
   if (payload.token === '') return '';
   try {
     const response = await fetch(`/graphql/refresh`);
+    if (!response.ok) {
+      setUserToken('');
+      return '';
+    }
     const { token } = await response.json();
     setUserToken(token);
     return token;
-  } catch {
-    setUserToken('');
+  } catch (err) {
     return '';
   }
 };
