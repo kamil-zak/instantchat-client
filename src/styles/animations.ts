ff77ded15feda0231cad2d225392d811dc2ead32
Kamil Żak <kamil.zak.pl@outlook.com>
2022-05-28 15:40:25 +0200

    Add fadeIn and rotate animations

--- src/styles/animations.ts
+++ src/styles/animations.ts
@@ -1,12 +1,30 @@
 import { keyframes } from 'styled-components';
 
 export const slideIn = (translate: number) => keyframes`
-from {
+    from {
        transform: translateY(${translate}px);
        opacity: 0;
    }
    to {
        transform: translateY(0px);
        opacity: 1;
+   }
+`;
+
+export const fadeIn = keyframes`    
+    from {
+       opacity: 0;
+   }
+   to {
+       opacity: 1;
+   }
+`;
+
+export const rotate = keyframes`
+from {
+       transform: rotate(0);
+   }
+   to {
+       transform: rotate(360deg);
    }
 `;
